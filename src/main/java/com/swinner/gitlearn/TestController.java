85039ac42890eb39ff2ba841f1f7c1795bca9ff9
tengfeigao2 <tengfeigao2@creditease.cn>
2019-09-19 12:02:23 +0800

    本地第八次提会滚到第五次

--- src/main/java/com/swinner/gitlearn/TestController.java
+++ src/main/java/com/swinner/gitlearn/TestController.java
@@ -10,7 +10,32 @@ public class TestController {
     @RequestMapping("/index")
     public String getIndex(){
         System.out.printf("dev 本地提交");
-        System.out.printf("dev 远程提交");
-        return "getIndex hello";
+
+        System.out.printf("github YUANCHGENBG提交");
+
+        System.out.printf("本地   第二次提交");
+
+        System.out.printf("本地   第三次提交");
+
+        System.out.printf("本地   第四次提交");
+
+        System.out.printf("远程   第四次提交");
+
+        System.out.printf("远程   第五次提交");
+
+
+        System.out.printf("本地   第五次提交");
+
+        System.out.printf("远程   第六次提交");
+
+        System.out.printf("本地   第六次提交  XIUGAI  第六次提交   又来一个第六次提交 提交");
+
+        System.out.printf("本地   第七次提交");
+
+        System.out.printf("本地   第八次提交  会滚到第五次");
+
+
+        return "getIndex hello  dddd  XXXXX";
+
     }
 }
