aed1dc8289ce00b962ea903fdefb5981cd8999d0
swinner <tengfei_gao@163.com>
2019-09-19 16:35:57 +0800

    Update GitlearnApplication.java
    add todo

--- src/main/java/com/swinner/gitlearn/GitlearnApplication.java
+++ src/main/java/com/swinner/gitlearn/GitlearnApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GitlearnApplication {
 
     public static void main(String[] args) {
+        //todo
         SpringApplication.run(GitlearnApplication.class, args);
     }
 
